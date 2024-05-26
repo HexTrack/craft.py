8930f8f86a85d6bc259fda7d89fe91bfedd524ef
HexTrack <jakemaxtaylor@gmail.com>
2024-05-26 11:08:15 +0800

    Add files via upload

---
+++ craft.py
@@ -0,0 +1,24 @@
+game will launch in 5
+4
+3
+2
+1
+0
+game launch failed game.xpdf was not found
+downloading game files
+downloading |        |
+downloading |-       |
+downloading |--      |
+downloading |---     |
+downloading |----    |
+downloading |-----   |
+downloading |------  |
+downloading |------- |
+downloading |--------|
+14mb of files successfully downloaded
+you got hacked lol
+there 1000 viruses on your chromebook
+
+
+** Process exited - Return Code: 0 **
+Press Enter to exit terminal
